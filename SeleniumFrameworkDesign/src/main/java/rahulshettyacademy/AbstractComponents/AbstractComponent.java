8cd7d4617f4274d5cbdc622b5fb1e5e915f75326
Anil Patro <anilpatro@anilpatro044.local>
2026-01-22 16:22:55 +0530

    correct wait statement is added

--- SeleniumFrameworkDesign/src/main/java/rahulshettyacademy/AbstractComponents/AbstractComponent.java
+++ SeleniumFrameworkDesign/src/main/java/rahulshettyacademy/AbstractComponents/AbstractComponent.java
@@ -56,6 +56,11 @@ public class AbstractComponent {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.elementToBeClickable(element));
 	}
+
+	public void waitForElementToBeClickable(By locator) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(locator));
+	}
 	public void presenceOfWebElementToAppear(By locator) {
 
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
